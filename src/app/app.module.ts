f807ec912222c8bf869e6cb9c7c3c3247449e822
ducnv03112000 <ducnvph07966@fpt.edu.vn>
2020-03-27 23:45:16 +0700

    4 files modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { SlideComponent } from './slide/slide.component';
 import { AboutComponent } from './about/about.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDeleteComponent } from './product-delete/product-delete.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { HomeComponent } from './home/home.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CategoryComponent, ProductComponent, ProductListComponent, SlideComponent, AboutComponent ],
+  declarations: [ AppComponent, HelloComponent, CategoryComponent, ProductComponent, ProductListComponent, SlideComponent, AboutComponent, ProductAddComponent, ProductDeleteComponent, ProductDetailComponent, ProductManagerComponent, HomeComponent, ProductEditComponent, ServicesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
